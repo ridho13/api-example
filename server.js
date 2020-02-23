966eafc5a0cc22844f67b6aad2e866705342d90e
Ridho Rahman Hariadi <ridho13@gmail.com>
2020-02-23 17:19:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var restify = require('restify'); 
 function respond(req, res, next) { 
-res.send('hello '+ req.params.name);
+res.send('new hello '+ req.params.name);
 } 
 var server = restify.createServer();
 server.get('/hello/:name', respond);
